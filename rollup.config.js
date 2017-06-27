c86239c84402527920110527b829924d6ed75797
fongelias <fongelias@gmail.com>
2017-06-26 21:08:09 -0400

    Routing update to accomodate mutiple layers

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 //Dependencies
 import babel from 'rollup-plugin-babel';
+import localResolve from 'rollup-plugin-local-resolve';
 //import eslint from 'rollup-plugin-eslint';
 
 //Module
@@ -8,6 +9,7 @@ export default {
 	format: 'cjs',
 	dest: 'tensor-lib.js',
 	plugins: [
+		localResolve(),
 		/*eslint({
 			exclude: [
 				'',
